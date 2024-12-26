92798e2bdd058eb17d2a649dbf037b60e192412a
fernandoG494 <lfgc851@gmail.com>
2024-12-26 12:32:11 +0100

    2024

---
+++ Javascript/objects.js
@@ -0,0 +1,22 @@
+function getCount(a, b) {
+  let counter = 0;
+  if (a == b) {
+    counter;
+  }
+}
+
+function main() {
+  let objects = [
+    [1, 1],
+    [2, 3],
+    [3, 3],
+    [3, 4],
+    [4, 5],
+  ];
+
+  for (let i = 0; i < objects.length; i++) {
+    getCount(objects[i][0], objects[i][1]);
+  }
+}
+
+main();
